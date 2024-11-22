0f08f58de1e49af39796df6229a1829c276402e1
Yago Pajariño <yagopajarino@outlook.com>
2024-11-22 16:54:57 -0300

    Agrega logo activia al form

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   return (
     <>
     <div className=" flex flex-col items-center pt-8">
-      <img src="https://www.ctl.tech/hubfs/logo-ctl-white.svg" className=" w-[100px] my-4"></img>
+      <div className="flex items-center justify-center">
+      <img src="https://www.ctl.tech/hubfs/logo-ctl-white.svg" className=" w-[100px] my-4 mr-8"></img>
+      <img src="https://raw.githubusercontent.com/ctl-omnicanalidad-devs/ctl-sorteo-2024/9e3b9053429b408586ddfd29b902776e3904871e/form/public/svg-image-1.svg?token=GHSAT0AAAAAACYJ5S7VJZGIISSUL3SGSYEUZ2A4DRQ" className=" w-[100px] my-4 ml-8"></img>
+      </div>
       <Form />
     </div>
     </>
