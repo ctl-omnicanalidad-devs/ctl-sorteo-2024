1c3d60b2428af05c18664f69a2f953cdb82f4da1
Yago Pajariño <yagopajarino@outlook.com>
2024-11-22 17:20:59 -0300

    Modifica front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -30,7 +30,8 @@ function App() {
         <GameScreen jugadores={jugadores} setJugadores={setJugadores} premios={premios} empresa={empresa}/> 
   ) : (
     <div className=" w-screen h-screen flex items-center justify-center flex-col bg-black21-100">
-      <img src="https://raw.githubusercontent.com/ctl-omnicanalidad-devs/ctl-sorteo-2024/refs/heads/main/front/public/frame.png"></img>
+      <p className="my-8 text-white text-4xl">Completa el formulario para participar del sorteo</p>
+      <img src="https://raw.githubusercontent.com/ctl-omnicanalidad-devs/ctl-sorteo-2024/refs/heads/main/front/public/frame.png" className="w-[600px]"></img>
       <button className="bg-black text-white px-4 py-2 rounded-lg my-8" onClick={() => setPassword("sorteofindeaño2024")}>Ingresar</button>
     </div>
   )
